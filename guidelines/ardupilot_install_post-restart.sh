9cd172d0330223beb224a15c666aca1dc4b02884
Dhruv Ahlawat <dhruvahlawat573@gmail.com>
2024-03-27 14:50:22 +0530

    updated ardupilot to ArduPilot

--- guidelines/ardupilot_install_post-restart.sh
+++ guidelines/ardupilot_install_post-restart.sh
@@ -1,5 +1,5 @@
-cd ardupilot #this changes the current directory to go to ardupilot. Ardupilot must be in the current directory for this to work.
+cd ArduPilot #this changes the current directory to go to ardupilot. Ardupilot must be in the current directory for this to work.
 
-./waf configure --board PixHawk1
+./waf configure --board PixHawk1 #Our flight controller board.
 ./waf copter
 sim_vehicle.py -v ArduCopter --map --console
--- guidelines/ardupilot_install_pre.sh
+++ guidelines/ardupilot_install_pre.sh
@@ -3,13 +3,15 @@
 #So this script also installs ardupilot for you directly, if no errors are encountered. It's best to understand what the individual commands do though, 
 #which you will when you learn bash and linux commands.
 
+# learn how to run bash scripts by searching online. Make sure you are in the Autonomous directory when running it.
+
 sudo apt update
 sudo apt upgrade -y
 
 sudo apt-get install git
 sudo apt-get install gitk git-gui -y
 
-git clone --recursive https://github.com/DhruvAhlawat/ardupilot.git
+git clone --recursive https://github.com/DhruvAhlawat/ardupilot.git ArduPilot
 cd ardupilot
 git submodule update --init --recursive
 
